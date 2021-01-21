ecc0512a31c82471cc56bcb1b0e1bbb5abf40011
sethltaylor <slt70@georgetown.edu>
2021-01-21 12:42:19 -0500

    Update List Fortress Data.R
    Fix errors with scraping list data and start cleaning datafile for use in Shiny app

--- List Fortress Data.R
+++ List Fortress Data.R
@@ -1,10 +1,11 @@
 library(httr)
 library(jsonlite)
+library(httr)
 library(dplyr)
 library(purrr)
 library(tidyr)
 library(tibble)
-library(stringr)
+library(lubridate)
 
 #Scraping list of tournaments
 tournaments <- GET("http://listfortress.com/api/v1/tournaments/")
@@ -18,24 +19,36 @@ urls <- paste0("http://listfortress.com/api/v1/tournaments/", tournaments$id)
 data <- lapply(urls, GET)
 
 #Pulling out content item from list of scraped urls
-test <- map(data, 6)
+data <- map(data, 6)
 #Converting content from JSON
-test <- lapply(test, function(x) fromJSON(rawToChar(x)))
+data <- lapply(data, function(x) fromJSON(rawToChar(x)))
 
 #Grabbing participants dataframe from each URL (12th item in list of lists) and binding it 
-participants <-do.call("rbind", lapply(test, "[[", 12))
+participants <-do.call("rbind", lapply(data, "[[", 12))
 
-#Merge location information back into participants list
+#Cleaning JSON for parsing
+participants$list_json <- trimws(participants$list_json)
+#Remove NAs
+participants <- participants[!is.na(participants$list_json),]
+participants <- participants[!(participants$list_json == ""),]
+#Removing one record with invalid json
+participants <- participants[!(participants$id == 3301),]
 
+#Trying to parse lists json
+participants$lists <- map(participants$list_json, fromJSON)
 
+#Grabbing factions
+participants$factions <- lapply(participants$lists, "[[", 'faction')
+#Grabbing points values 
+participants$points <- lapply(lists, "[[", 'points')
 
-#Old testing code for one tournament 
-#nznat <- GET("http://listfortress.com/api/v1/tournaments/261")
-#nznat <- fromJSON(rawToChar(nznat$content))
-#data <- nznat$participants
+#Dropping list columns
+participants <- select(participants, 1:9, 13,14)
 
-#data$points <- str_match(data$list_json, "(?<=\\points\\\":d+")
+#Merge back in tournament location and date
 
-#nznat_raw <- content(nznat)
-#data_raw <- enframe(unlist(nznat_raw))
+participants <- merge(participants, tournaments[,c('id', 'state', 'country', 'date', 'format_id')], by.x = 'tournament_id', by.y = 'id', all.x = T)
+
+#Convering dates
+participants$date <- as.Date(participants$date, "%Y-%m-%d")
 
